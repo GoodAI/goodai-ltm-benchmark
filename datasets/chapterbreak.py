c8d42844f5eb334a64a6bb433f2cafabc5fb3a26
David <david.castillo@users.noreply.github.com>
2024-02-21 10:26:15 +0000

    GoodAI split for ChapterBreak dataset, with a selection of hand-cured samples

--- datasets/chapterbreak.py
+++ datasets/chapterbreak.py
@@ -1,10 +1,11 @@
+import re
 import json
 import random
 from inspect import getsource
 from typing import List, Tuple
 
-from utils.data import get_gdrive_file
-from dataclasses import dataclass
+from utils.data import get_gdrive_file, get_data_path
+from dataclasses import dataclass, field
 from utils.ui import ordinal
 from utils.tokens import token_len
 from dataset_interfaces.interface import DatasetInterface, TestExample
@@ -13,26 +14,6 @@ from dataset_interfaces.interface import DatasetInterface, TestExample
 # Extracted from gdrive folder
 # https://drive.google.com/drive/folders/1JkFHspT56_yRWwXVj47Fw0PzHtitODt5
 GDRIVE_8K_ID = "15AcGiC4wIglru2gK2MHSX5Fie7gYxTTS"
-
-extraction_prompt_template = """
-You are an expert language analyst who helps in finding connections between different pieces of text within the same document. As input, you will receive some text, which corresponds to the final pages of a chapter, and you will analyze its content in order to find elements that point to the true next-chapter begin as the only option.
-
-Respond with a JSON listing the elements like this:
-[
-  {
-    "reason": "why the element links to the true next-chapter beginning and to no other chapter-beginning",
-    "element": "exact string that can be found in the text",
-  },
-  ...
-]
-
-Respond with an empty list if you find no unique connection.
-
-You know that the true next-chapter beginning is this:
-{ctx}
-
-And you also know that there exist these other chapter beginnings, which are not from the next chapter, despite being disguised as such:
-""".strip()
 
 
 def split_in_pages(text: str, max_tokens_per_split: int) -> list[str]:
@@ -73,16 +54,18 @@ def deliver_in_pages(text: str, max_page_tokens: int, first_page_prefix: str = "
 class ChapterBreakDataset(DatasetInterface):
     name: str = "ChapterBreak"
     description: str = (
-        "The agent is given a text that corresponds to the final part of a book "
-        "chapter, plus 6 possible continuations. It is then asked to say which of the "
-        "six continuations is the right one."
+        "The agent is given a text that corresponds to the content of many book chapters, in addition to 6 possible "
+        "beginnings for the next chapter. It is then asked to say which of the six continuations is the right one."
     )
-    split: str = "ao3"  # pg19 / ao3 / all
-    page_tokens: int = 1024 - 20  # Leave some margin for text decorations
     reset_message: str = "Forget the current chapter and its potential continuations."
+    # The GoodAI split is a selection of samples that have been inspected by us to ensure that they are solvable, also
+    # removing ordering hints from the chapter titles.
+    split: str = "goodai"  # goodai / pg19 / ao3 / all
+    max_message_tokens: int = 1024
+    selection_info: dict = field(default_factory=dict)
 
     def __post_init__(self):
-        assert self.split in {"pg19", "ao3", "all"}
+        assert self.split in {"goodai", "pg19", "ao3", "all"}
 
     def load_data(self) -> dict:
         filename = f"chapterbreak_ctx_8192.json"
@@ -90,13 +73,35 @@ class ChapterBreakDataset(DatasetInterface):
         with open(path) as fd:
             return json.load(fd)
 
+    def apply_sample_selection(self, samples: dict) -> dict:
+        with open(get_data_path(self.name, "chapterbreak-goodai-selection.json")) as fd:
+            selection = json.load(fd)
+        sample_selection = dict()
+        for sel in selection:
+            if not sel["solvable"]:
+                continue
+            sample = samples[sel["id"]]
+            reg_expr = sel.get("chapter_cleanup", None)
+            if reg_expr:
+                right_value = re.match(reg_expr, sample["pos"]).group(1)
+                false_beginnings = sample["negs"]
+                for i in range(len(false_beginnings)):
+                    wrong_value = re.match(reg_expr, false_beginnings[i]).group(1)
+                    if wrong_value is not None:
+                        false_beginnings[i] = false_beginnings[i].replace(wrong_value, right_value)
+            sample_selection[sel["id"]] = sample
+            self.selection_info[sel["id"]] = sel
+        return sample_selection
+
     def get_samples(self, raw_dataset: dict) -> list[dict]:
         samples = dict()
-        split_keys = ["pg19", "ao3"] if self.split == "all" else [self.split]
+        split_keys = ["pg19", "ao3"] if self.split in ["goodai", "all"] else [self.split]
         for split in split_keys:
             for book_id, chapter_samples in raw_dataset[split].items():
                 for i, chapter in enumerate(chapter_samples):
                     samples[f"{book_id}_{i}"] = chapter
+        if self.split == "goodai":
+            samples = self.apply_sample_selection(samples)
         sample_list = [samples[k] | {"id": k} for k in sorted(samples.keys())]
         random.Random(self.seed).shuffle(sample_list)
         return sample_list
@@ -111,12 +116,13 @@ class ChapterBreakDataset(DatasetInterface):
             random.Random(self.seed + sample_idx).shuffle(beginnings)
 
             script = ["I am going to read you the final pages of a book chapter. Okay?"]
-            script.extend(deliver_in_pages(sample["ctx"], self.page_tokens))
+            max_page_content_tokens = self.max_message_tokens - 20  # Leave some margin for text decorations
+            script.extend(deliver_in_pages(sample["ctx"], max_page_content_tokens))
 
             answer = 0
             script.append(f"Now I will give you {len(beginnings)} options for the beginning of the next chapter. Ready?")
             for i, (is_true_suffix, option) in enumerate(beginnings):
-                script.extend(deliver_in_pages(option, self.page_tokens, first_page_prefix=f"Option {i + 1}. "))
+                script.extend(deliver_in_pages(option, max_page_content_tokens, first_page_prefix=f"Option {i + 1}. "))
                 if is_true_suffix:
                     answer = i + 1
             assert answer > 0
@@ -146,19 +152,41 @@ class ChapterBreakDataset(DatasetInterface):
         return example_list
 
     def answer_statement_idx(self, example: TestExample) -> tuple[int, int]:
-        # TODO: try to figure out where the relevant information actually is
-        # TODO: load a sample from the dataset in the Python terminal, and see if I can have GPT help me extract key info
-        # TODO: from the gold answer and then find it in the last pages of the chapter.
-        # For now, we'll just assume that the whole chapter ending is relevant.
+
+        # Find the message that goes after the last context page
         last_page_idx = None
         for i, script_line in enumerate(example.script):
             if script_line.endswith("options for the beginning of the next chapter. Ready?"):
                 last_page_idx = i - 1
                 break
         assert last_page_idx is not None
-        script_answer_index = last_page_idx
-        answer_end_char = len(example.script[last_page_idx])
-        return script_answer_index, answer_end_char
+        context_script = example.script[1:last_page_idx + 1]
+
+        # GoodAI-selected samples contain meta-data for these cases
+        # Find the latest appearance of relevant information
+        if example.example_id in self.selection_info:
+            relevant_sentences = self.selection_info[example.example_id]["ctx"]
+            script_index = char_index = None
+            for line_idx, script_line in reversed(list(enumerate(context_script))):
+                for sentence in relevant_sentences:
+                    i = script_line.find(sentence)
+                    if i < 0:
+                        continue
+                    last_char_i = i + len(sentence)
+                    if script_index is None or char_index < last_char_i:
+                        script_index, char_index = line_idx, last_char_i
+                if script_index is not None:
+                    return script_index + 1, char_index
+
+        # Otherwise, we'll just assume that the key information lies somewhere around the middle of the context.
+        middle_point_chars = sum(len(msg) for msg in context_script) // 2
+        counted_chars = 0
+        for script_answer_index, script_line in enumerate(context_script):
+            counted_chars += len(script_line)
+            if counted_chars > middle_point_chars:
+                break
+        answer_end_char = counted_chars - middle_point_chars
+        return script_answer_index + 1, answer_end_char
 
     def evaluate_correct(
         self, questions: List[str], responses: List[str], expected_answers: List[str]
--- utils/openai.py
+++ utils/openai.py
@@ -36,6 +36,8 @@ def get_max_prompt_size(model: str):
     assert model in allowed_models()
     if model == "gpt-4-1106-preview":
         return 128_000
+    if model == "gpt-3.5-turbo-0125":
+        return 16_384
     if "32k" in model:
         return 32_768
     if "16k" in model:
@@ -50,6 +52,8 @@ def get_max_prompt_size(model: str):
 def token_cost(model: str) -> tuple[float, float]:
     if model == "gpt-3.5-turbo-instruct":
         return 0.000_001_5, 0.000_002
+    if model == "gpt-3.5-turbo-0125":
+        return 0.000_000_5, 0.000_001_5
     if model == "gpt-4-1106-preview":
         return 0.000_01, 0.000_03
     if model.startswith("gpt-4-32k"):
@@ -103,6 +107,7 @@ def ask_llm(
     context_length: int = None,
     cost_callback: Callable[[float], None] = None,
     timeout: float = 300,
+    max_tokens: Optional[int] = None,
 ) -> str:
     set_api_key()
     model = get_model(model)
@@ -112,6 +117,7 @@ def ask_llm(
         messages=context,
         temperature=temperature,
         timeout=timeout,
+        max_tokens=max_tokens,
     )
     if cost_callback is not None:
         cost_callback(response_cost(response))
