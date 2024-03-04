5f8bcb36570480f68e6e3c4aada41adb1828a23d
Joseph Davidson <joseph.davidson@goodai.com>
2024-03-04 09:20:04 +0100

    first version

--- datasets/spy_meeting.py
+++ datasets/spy_meeting.py
@@ -1,45 +1,119 @@
 from dataclasses import dataclass
+from random import choice
 from typing import Tuple, List
 
-from dataset_interfaces.interface import DatasetInterface, TestExample
+from faker import Faker
 
-CODED_STATEMENTS = [
-    "The house of red is burning today.",
-    "The rabbits of Stalingrad run freely this time of year.",
-    "The eagle is awakening.",
-    "Birds are in the branches.",
-    "The blues are in disarray.",
-    "Our father is disappointed.",
-    "Our mutual friends are waiting for an answer.",
-    "What are your colleagues telling you?"
+from dataset_interfaces.interface import DatasetInterface, TestExample, WaitCreator
+
+
+PLACE_TEMPLATE = "{}: We will rendezvous {}."
+TIME_TEMPLATE = "{}: The time we will meet is {}."
+AMOUNT_TEMPLATE = "{}: Bring to the meeting {}."
+
+CODED_INFO_PLACE = [
+    ("where the land meets the sea", ["shore", "beach", "coastline"]),
+    ("where the occupiers supply their trains", ["railway depot", "train yard"]),
+    ("where the cars are made", ["automobile factory", "car factory"]),
+    ("where the apples grow", ["orchard"]),
+    ("at the most geometric part of the city", ["city square"])
 ]
+
+CODED_INFO_TIME = [
+    ("when the sun starts its travel across the sky", ["sunrise", "dawn"]),
+    ("when the blackbirds sing", ["dawn", "sunrise"]),
+    ("when the sun is high", ["noon"]),
+    ("when the sun leaves the sky", ["sunset", "dusk"]),
+    ("when the moon is high", ["night"]),
+]
+
+CODED_INFO_AMOUNT = [
+    ("a way to get across a river", ["boat", "bridge"]),
+    ("a quiet way to open locked doors", ["lockpicks", "master key"]),
+    ("a way to persuade the border guards to let us through", ["bribe", "credentials"]),
+    ("a fast land escape vehicle", ["motorbike", "fast car"]),
+]
+
 
 @dataclass
 class SpyMeetingDataset(DatasetInterface):
     name: str = "Spy Meeting"
-    description: str = "Have a cold war esque park bench meeting with a discussion in coded language. Then have the agent try to remember this strange conversation."
-    question: str = "What is my favourite colour?"
-
+    description: str = "The agent is given three clandestine messages, "
+    question: str = "Given the clandestine messages you have recieved, tell me when and where a meeting is going to happen and what you should bring."
 
     def generate_examples(self, num_examples):
         examples = []
+        faker = Faker(["en_US", "en_IE"])
 
         for _ in range(num_examples):
-            is_question = []
-            script = ["You are on a park bench in a cold January. Someone sits down beside you, opens a newspaper, and starts a conversation with you."]
-            num_statements = 3
+            names = []
+            expected_responses = []
+            messages = []
+            faker.unique.clear()
+            for k in range(3):
+                names.append(faker.unique.name())
 
-            coded_copy =
-            for stmt_idx in range(num_statements):
-                  script.append()
+            waits = [WaitCreator.create_wait(percentage_finished=10)]
+            is_question = [False]
+            script = [f"You will be given three messages from different people {names[0]}, {names[1]}, and {names[2]}."]
+            topic_list = [(CODED_INFO_TIME, TIME_TEMPLATE), (CODED_INFO_AMOUNT, AMOUNT_TEMPLATE), (CODED_INFO_PLACE, PLACE_TEMPLATE)]
 
+            for k in range(3):
+                name = choice(names)
+                names.remove(name)
+                topic = choice(topic_list)
+                topic_list.remove(topic)
 
+                # A topic is a pair of coded statements and a template
+                potential_messages, template = topic
 
+                coded_message, potential_interpretations = choice(potential_messages)
+                message = template.format(name, coded_message)
+
+                messages.append(message)
+                script.append(message)
+
+                is_question.append(False)
+                expected_responses.append(potential_interpretations)
+
+                if k == 2:
+                    waits.append(WaitCreator.create_wait(percentage_finished=90))
+                else:
+                    waits.append(WaitCreator.create_wait(percentage_finished=k+2 * 10))
+
+            script.append(self.question)
+            is_question.append(True)
+            waits.append(waits)
+
+            examples.append(TestExample(
+                dataset_generator=self,
+                script=script,
+                waits=waits,
+                is_question=is_question,
+                expected_responses=expected_responses
+            ))
+
+        return examples
 
     def evaluate_correct(
             self, questions: List[str], responses: List[str], expected_answers: List[str]
     ) -> Tuple[int, int, List[str]]:
-        pass
+
+        reasoning = []
+        response = responses[0]
+        correct = 1
+        for potential_answers in expected_answers:
+            found = False
+            for pa in potential_answers:
+                if pa in response:
+                    found = True
+                    break
+            if not found:
+                correct = 0
+                reasoning.append(f"{potential_answers} not found in answer.")
+
+        return correct, 1, reasoning
 
     def answer_statement_idx(self, example: TestExample) -> Tuple[int, int]:
-        pass
+        # Second statement in the script, character 0
+        return 1, 0
