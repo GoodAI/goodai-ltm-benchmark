f0ae257c3e21a3ad73301c461d28be52b0bc36cc
David <david.castillo@users.noreply.github.com>
2024-03-20 10:09:15 +0000

    Re-evaluation of Locations Directions tests of Benchmark 2 and generation of reports

--- reporting/generate_all_detailed_reports.py
+++ reporting/generate_all_detailed_reports.py
@@ -2,13 +2,10 @@ import os
 
 from reporting.detailed_report import _main
 
-target_tests = ["Benchmark 1 - 1k Filler", "Benchmark 1 - 10k Filler"]
+run_name_list = ["Benchmark 2 - 1k Filler", "Benchmark 2 - 10k Filler"]
 
 
-for t in target_tests:
-    path = f"data/tests/{t}/results"
+for run_name in run_name_list:
+    path = f"data/tests/{run_name}/results"
     for agent in os.scandir(path):
-        try:
-            _main(t, agent.name, None)
-        except:
-            continue
+        _main(run_name, agent.name, f"Detailed Report - {run_name} - {agent.name}")
