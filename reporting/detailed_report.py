c98dc8bb24c874c8af9570823de16d2079d244dc
David <david.castillo@users.noreply.github.com>
2024-03-21 13:16:35 +0000

    Add anchor IDs to detailed reports. Make pop-ups optional.

--- reporting/detailed_report.py
+++ reporting/detailed_report.py
@@ -7,14 +7,15 @@ from reporting.generate import gather_results, generate_report
 @click.argument("run_name", type=str)
 @click.argument("agent_name", type=str)
 @click.option("-o", "--output", type=str, required=False, default=None, help="Name of the resulting report, without extension.")
-def main(run_name: str, agent_name: str, output: str):
+def main(run_name: str, agent_name: str, output: str, show: bool):
     _main(run_name, agent_name, output)
 
 
-def _main(run_name: str, agent_name: str, output: str):
+def _main(run_name: str, agent_name: str, output: str, show: bool = True):
     benchmark_data, results = gather_results(run_name, agent_name)
     report_path = generate_report(results, output_name=output)
-    webbrowser.open_new_tab(report_path.as_uri())
+    if show:
+        webbrowser.open_new_tab(report_path.as_uri())
 
 
 if __name__ == "__main__":
