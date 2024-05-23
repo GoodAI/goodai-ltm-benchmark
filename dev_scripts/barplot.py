5c15e1b49b187725a817e3f564114af982b95050
David <david.castillo@users.noreply.github.com>
2024-05-23 17:04:21 +0100

    Updated barplot

--- dev_scripts/barplot.py
+++ dev_scripts/barplot.py
@@ -1,12 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS
-from collections import defaultdict
 from reporting.generate import get_summary_data
-from runner.master_log import MasterLog
-from utils.constants import DATA_DIR, EventType
-from utils.llm import tokens_in_text
-from utils.math import mean_std
-import numpy as np
 from matplotlib.patches import Patch
 
 
@@ -17,43 +11,60 @@ aliases = {
 
 
 def get_data(run_name: str, agent_name: str):
-    try:
-        return get_summary_data(run_name, agent_name)
-    except IndexError:
-        llm_name = agent_name.split(" - ")[1]
-        new_agent_name = agent_name.replace(llm_name, aliases[llm_name])
-        return get_summary_data(run_name, new_agent_name)
+    # TODO: Add the corresponding result files
+    if run_name.endswith("1k") and "Llama-3-70B" in agent_name:
+        return dict(score=1.9, score_std=2.3)
+    for i in range(2):
+        try:
+            return get_summary_data(run_name, agent_name)
+        except IndexError:
+            if i == 2:
+                return
+            llm_name = agent_name.split(" - ")[1]
+            if llm_name not in aliases:
+                return
+            agent_name = agent_name.replace(llm_name, aliases[llm_name])
 
 
 def main():
-    run_names = [(f"Benchmark 3 - {span}k", span) for span in [120, 200, 500]]
-    x_labels = [f"{span}k" for _, span in run_names]
-    # TODO: Rename all result dirs that have "gpt-4-turbo" or "claude-3-opus" to the exact name.
-    # TODO: See if that is already fixed for new results.
-
     llm_names = [
+        "together_ai-mistralai-Mixtral-8x7B-Instruct-v0.1",
+        "together_ai-mistralai-Mixtral-8x22B-Instruct-v0.1",
+        "huggingface-gradientai-Llama-3-70B-Instruct-Gradient-262k",
+        "gpt-3.5-turbo",
         "gpt-4-turbo-2024-04-09",
+        "gpt-4o",
         "claude-3-opus-20240229",
-        "together_ai-meta-llama-Llama-3-70b-chat-hf",
     ]
     llm_legend_names = [
-        "GPT-4 turbo",
-        "Claude 3 Opus",
+        "Mixtral 8x7B",
+        "Mixtral 8x22B",
         "Llama 3 70B",
+        "GPT-3.5 turbo",
+        "GPT-4 turbo",
+        "GPT-4o",
+        "Claude 3 Opus",
     ]
     llm_colors = {name: color for name, color in zip(llm_names, TABLEAU_COLORS.keys())}
+    llm_colors["together_ai-meta-llama-Llama-3-70b-chat-hf"] = llm_colors["huggingface-gradientai-Llama-3-70B-Instruct-Gradient-262k"]
     agent_names = [
+        "LLMChatSession - together_ai-mistralai-Mixtral-8x7B-Instruct-v0.1 - 32000",
+        "LLMChatSession - together_ai-mistralai-Mixtral-8x22B-Instruct-v0.1 - 32000",
+        "HFChatSession - huggingface-gradientai-Llama-3-70B-Instruct-Gradient-262k - 32768",
+        "LLMChatSession - gpt-3.5-turbo - 16384",
         "LLMChatSession - gpt-4-turbo - 128000",
+        "LLMChatSession - gpt-4o - 128000",
         "LLMChatSession - claude-3-opus - 200000",
+        "LTMAgentWrapper - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000 - QG_JSON_USER_INFO",
+        "LTMAgentWrapper - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000 - SEMANTIC_ONLY",
         "LTMAgentWrapper - gpt-4-turbo - 16384 - QG_JSON_USER_INFO",
         "LTMAgentWrapper - gpt-4-turbo - 16384 - SEMANTIC_ONLY",
         "LTMAgentWrapper - claude-3-opus - 16384 - QG_JSON_USER_INFO",
         "LTMAgentWrapper - claude-3-opus - 16384 - SEMANTIC_ONLY",
-        "LTMAgentWrapper - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000 - QG_JSON_USER_INFO",
-        "LTMAgentWrapper - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000 - SEMANTIC_ONLY",
     ]
     bar_groups = [
-        #(32, "agent_name"),
+        (2, agent_names),
+        (32, agent_names),
         (120, agent_names),
         (200, agent_names),
         (500, agent_names),
@@ -63,13 +74,15 @@ def main():
     x_ticks_pos = list()
     current_pos = 0
     for span, group_agents in bar_groups:
-        run_name = f"Benchmark 3 - {span}k"
+        run_name = f"Benchmark 3 - {span if span > 2 else 1}k"
         print(run_name)
-        x_ticks_labels.append(f"{span}k")
-        x_ticks_pos.append(current_pos + 0.5 * len(group_agents))
+        start_pos = current_pos
+        num_group_agents = 0
         for agent_name in group_agents:
             print(agent_name)
             data = get_data(run_name, agent_name)
+            if data is None:
+                continue
             llm = agent_name.split(" - ")[1]
             llm = aliases.get(llm, llm)
             kwargs = dict()
@@ -82,6 +95,9 @@ def main():
             color = llm_colors[llm]
             plt.bar(current_pos, data["score"], color=color, **kwargs)
             current_pos += 1
+            num_group_agents += 1
+        x_ticks_labels.append(f"{span}k")
+        x_ticks_pos.append(start_pos + 0.5 * num_group_agents)
         current_pos += 1
 
     # Manual legend
