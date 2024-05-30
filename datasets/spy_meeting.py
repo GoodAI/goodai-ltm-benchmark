e94c4739eca550091fb9b9c0e258605609928451
Joseph Davidson <joseph.davidson@goodai.com>
2024-05-30 11:22:21 +0200

    improved spy meeting eval

--- datasets/spy_meeting.py
+++ datasets/spy_meeting.py
@@ -1,3 +1,4 @@
+import re
 from dataclasses import dataclass
 from typing import Tuple, List
 
@@ -18,10 +19,10 @@ CODED_INFO_PLACE = [
 ]
 
 CODED_INFO_TIME = [
-    ("when the sun starts its travel across the sky", ["sunrise", "dawn", "rise"]),
+    ("when the sun starts its travel across the sky", ["sunrise", "dawn", "morning", "rise"]),
     ("when the blackbirds sing", ["dawn", "sunrise", "morning", "rise"]),
     ("when the sun is high", ["noon", "midday"]),
-    ("when the sun leaves the sky", ["sunset", "dusk", "set"]),
+    ("when the sun leaves the sky", ["sunset", "dusk", "sets"]),
     ("when the moon is high", ["night", "midnight"]),
 ]
 
@@ -84,26 +85,47 @@ class SpyMeetingDataset(DatasetInterface):
 
     def evaluate_correct(
             self, questions: List[str], responses: List[str], expected_answers: List[str]
-    ) -> Tuple[int, int, List[str]]:
+    ) -> Tuple[float, int, List[str]]:
 
         reasoning = []
         response = responses[0]
-        score = 0
+        correct_score = 0
+        other_answers = self.get_answers_for_others(expected_answers)
+
         for potential_answers in expected_answers:
             found = False
             for pa in potential_answers:
-                if pa in response:
+                regex = re.compile(rf"\b{pa}\b")
+                if len(regex.findall(response)) > 0:
                     found = True
                     break
             if not found:
                 reasoning.append(f"{potential_answers} not found in answer.")
             else:
                 reasoning.append("Answer contains expected keyword(s)")
-                score += 0.33
+                correct_score += 1
 
-        if score == 0.99:
-            score = 1
+        # Check to see if the agent is confused
+        confusion_score = 0
+        for incorrect_set in other_answers:
+            for incorrect_term in incorrect_set:
+                regex = re.compile(rf"\b{incorrect_term}\b")
+                if len(regex.findall(response)) > 0:
+                    reasoning.append(f"Answer also contains `{incorrect_term}`, which indicates that the agent has recalled something incorrect.")
+                    confusion_score = 1
+
+            # You only get marked down once for this
+            if confusion_score == 1:
+                break
+
+        score = max((correct_score - confusion_score) / 3, 0.0)
 
         return score, 1, reasoning
 
+    def get_answers_for_others(self, expected_answers):
+        other_answers = []
+        for tup in CODED_INFO_TIME + CODED_INFO_THING + CODED_INFO_PLACE:
+            if tup[1] not in expected_answers:
+                other_answers.append(tup[1])
 
+        return other_answers
