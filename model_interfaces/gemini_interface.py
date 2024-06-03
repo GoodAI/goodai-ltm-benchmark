e8197742da7caa087da9291245da707cb0225e72
David <david.castillo@users.noreply.github.com>
2024-06-03 14:02:08 +0100

    Google's python package is faulty. Using cURL instead.

--- model_interfaces/gemini_interface.py
+++ model_interfaces/gemini_interface.py
@@ -1,6 +1,8 @@
 import os
 import time
 import pickle
+import requests
+import json
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from google.ai import generativelanguage as glm
@@ -10,9 +12,40 @@ from dataclasses import dataclass
 from model_interfaces.interface import ChatSession
 
 
+def count_tokens_by_curl(text: str) -> int:
+    api_key = os.getenv('GOOGLE_API_KEY')
+    url = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:countTokens?key={api_key}'
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post(url, headers=headers, data=json.dumps({"contents": [{"parts": [{"text": text}]}]}))
+    return r.json()["totalTokens"]
+
+
+def reply_by_curl(history: list[glm.Content]) -> str:
+    api_key = os.getenv('GOOGLE_API_KEY')
+    url = f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-latest:generateContent?key={api_key}'
+    headers = {'Content-Type': 'application/json'}
+    contents = [{"role": msg.role, "parts": [{"text": p.text} for p in msg.parts]} for msg in history]
+    safety = [
+        {"category": f"HARM_CATEGORY_{cat}", "threshold": "BLOCK_NONE"}
+        for cat in ["HARASSMENT", "HATE_SPEECH", "SEXUALLY_EXPLICIT", "DANGEROUS_CONTENT"]
+    ]
+    gen_config = {"temperature": 0.0}
+    data = {"contents": contents, "safetySettings": safety, "generationConfig": gen_config}
+    r = requests.post(url, headers=headers, data=json.dumps(data)).json()
+    if "error" in r and r["error"]["code"] == 503:  # The model is overloaded. Please try again later.
+        print(r)
+        time.sleep(10)
+        return reply_by_curl(history)
+    try:
+        return r["candidates"][0]["content"]["parts"][0]["text"]
+    except:
+        print(r)
+        raise
+
+
 @dataclass
 class GeminiProInterface(ChatSession):
-    is_local: bool = True  # It is free for now
+    is_local: bool = True  # TODO: Capture cost information
 
     @property
     def history_path(self) -> Path:
@@ -25,24 +58,12 @@ class GeminiProInterface(ChatSession):
         self.reset()
 
     def reply(self, user_message: str, agent_response: Optional[str] = None) -> str:
-        if agent_response is not None:
-            self.chat.history.append(glm.Content({'role': 'user', 'parts': [glm.Part({"text": user_message})]}))
-            self.chat.history.append(glm.Content({'role': 'model', 'parts': [glm.Part({"text": agent_response})]}))
-            return agent_response
-        response = self.chat.send_message(
-            user_message,
-            safety_settings={
-                cat: HarmBlockThreshold.BLOCK_NONE
-                for cat in [
-                    HarmCategory.HARM_CATEGORY_HARASSMENT,
-                    HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-                    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                ]
-            }
-        ).text
-        time.sleep(30)  # 2 requests per minute in the free tier
-        return response
+        self.chat.history.append(glm.Content({'role': 'user', 'parts': [glm.Part({"text": user_message})]}))
+        if agent_response is None:
+            agent_response = reply_by_curl(self.chat.history)
+        self.chat.history.append(glm.Content({'role': 'model', 'parts': [glm.Part({"text": agent_response})]}))
+        time.sleep(0.1)
+        return agent_response
 
     def reset(self, history=None):
         self.chat = self.model.start_chat(history=history or [])
@@ -56,10 +77,6 @@ class GeminiProInterface(ChatSession):
             self.reset(pickle.load(fd))
 
     def token_len(self, text: str) -> int:
-        for i in range(3):
-            try:
-                return self.model.count_tokens(text, request_options={"timeout": 10000}).total_tokens
-            except:  # google.api_core.exceptions.DeadlineExceeded:
-                if i == 2:
-                    raise
-                time.sleep(5)
+        tokens = count_tokens_by_curl(text)
+        time.sleep(0.1)
+        return tokens
