9c31f7f4e03c06b6af6f788e3fd9c4379955ac6a
David <david.castillo@users.noreply.github.com>
2024-06-04 17:37:23 +0100

    Load plot info from CSV for easier use

--- dev_scripts/barplot.py
+++ dev_scripts/barplot.py
@@ -1,117 +1,81 @@
+from collections import OrderedDict
 import matplotlib.pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS
-from reporting.generate import get_summary_data
 from matplotlib.patches import Patch
 
 
 PLOT_STD = False
 
-
 aliases = {
-    "gpt-4-turbo": "gpt-4-turbo-2024-04-09",
-    "claude-3-opus": "claude-3-opus-20240229",
+    "Mixtral-8x7B-Instruct-v0.1": "Mixtral 8x7B",
+    "Mixtral-8x22B-instruct-v0.1": "Mixtral 8x22B",
+    "Meta Llama 3 70B Instruct": "Llama 3 70B",
+    "gpt-3.5-turbo": "GPT-3.5 turbo",
+    "gpt-4-turbo-2024-04-09": "GPT-4 turbo",
+    "gpt-4o": "GPT-4o",
+    "Claude Opus": "Claude 3 Opus",
+    "Gemini 1.5 Pro": "Gemini 1.5 Pro",
+    "LTM Agent 1 Meta Llama 3 70B Instruct": "LTM Llama 3 70B",
+    "LTM Agent 1 gpt-4-turbo-2024-04-09": "LTM GPT-4 turbo",
+    "LTM Agent 1 Opus": "LTM Claude 3 Opus"
 }
 
 
-def get_data(run_name: str, agent_name: str):
-    for i in range(2):
-        try:
-            return get_summary_data(run_name, agent_name)
-        except IndexError:
-            if i == 2 or " - " not in agent_name:
-                return
-            llm_name = agent_name.split(" - ")[1]
-            if llm_name not in aliases:
-                return
-            agent_name = agent_name.replace(llm_name, aliases[llm_name])
+def load_csv_data() -> OrderedDict:
+    results = OrderedDict()
+    csv_path = input("Enter the path to the CSV results: ")
+    with open(csv_path) as fd:
+        for line in fd:
+            if line[:2] in ["Is", "1k", "32", "12", "20", "50"]:
+                k = line.split(" ")[0]
+                if k == "1k":
+                    k = "2k"
+                results[k] = dict()
+            elif line[:3] in ["gpt", "Cla", "LTM", "Mix", "Met", "Gem"]:
+                model, _, score, std = line.split(",")[:4]
+                score = score.strip()
+                std = std.strip()
+                score = float(score) if score != "" else 0
+                std = float(std) if std != "" else 0
+                model = aliases[model]
+                results[k][model] = dict(score=score, score_std=std)
+            if line.startswith("Others not included"):
+                break
+    return results
 
 
 def main():
-    llm_names = [
-        "together_ai-mistralai-Mixtral-8x7B-Instruct-v0.1",
-        "together_ai-mistralai-Mixtral-8x22B-Instruct-v0.1",
-        "together_ai-meta-llama-Llama-3-70b-chat-hf",
-        "gpt-3.5-turbo",
-        "gpt-4-turbo-2024-04-09",
-        "gpt-4o",
-        "claude-3-opus-20240229",
-        "gemini",
-    ]
-    llm_legend_names = [
-        "Mixtral 8x7B",
-        "Mixtral 8x22B",
-        "Llama 3 70B",
-        "GPT-3.5 turbo",
-        "GPT-4 turbo",
-        "GPT-4o",
-        "Claude 3 Opus",
-        "Gemini 1.5 Pro",
-    ]
-    llm_colors = {name: color for name, color in zip(llm_names, TABLEAU_COLORS.keys())}
-    agent_names = [
-        "LLMChatSession - together_ai-mistralai-Mixtral-8x7B-Instruct-v0.1 - 32000",
-        "LLMChatSession - together_ai-mistralai-Mixtral-8x22B-Instruct-v0.1 - 32000",
-        "LLMChatSession - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000",
-        "LLMChatSession - gpt-3.5-turbo - 16384",
-        "LLMChatSession - gpt-4-turbo - 128000",
-        "LLMChatSession - gpt-4o - 128000",
-        "LLMChatSession - claude-3-opus - 200000",
-        "GeminiProInterface",
-        "LTMAgentWrapper - together_ai-meta-llama-Llama-3-70b-chat-hf - 8000 - QG_JSON_USER_INFO",
-        "LTMAgentWrapper - gpt-4-turbo - 16384 - QG_JSON_USER_INFO",
-        "LTMAgentWrapper - claude-3-opus - 16384 - QG_JSON_USER_INFO",
-    ]
-    bar_groups = [
-        (0, agent_names),
-        (2, agent_names),
-        (32, agent_names),
-        (120, agent_names),
-        (200, agent_names),
-        (500, agent_names),
-    ]
-
     x_ticks_labels = list()
     x_ticks_pos = list()
     current_pos = 0
-    for span, group_agents in bar_groups:
-        label = {
-            0: "Isolated",
-            2: "1k"
-        }.get(span, f"{span}k")
-        run_name = f"Benchmark 3 - {label}"
-        print(run_name)
+    llm_colors = None
+    llm_names = None
+    for span_label, agent_values in load_csv_data().items():
+        if llm_colors is None:
+            llm_names = [name for name in agent_values.keys() if not name.startswith("LTM")]
+            llm_colors = {name: color for name, color in zip(llm_names, TABLEAU_COLORS.keys())}
         start_pos = current_pos
         num_group_agents = 0
-        for agent_name in group_agents:
-            print(agent_name)
-            data = get_data(run_name, agent_name)
-            if data is None:
-                continue
-            llm = agent_name.split(" - ")[1]
-            llm = aliases.get(llm, llm)
+        for agent_name, result in agent_values.items():
             kwargs = dict()
-            if agent_name.startswith("LTMAgentWrapper"):
+            if agent_name.startswith("LTM"):
                 kwargs["edgecolor"] = "white"
-            if agent_name.endswith("SEMANTIC_ONLY"):
-                kwargs["hatch"] = "/"
-            elif agent_name.endswith("QG_JSON_USER_INFO"):
                 kwargs["hatch"] = "//"
-            color = llm_colors[llm]
-            score = data["score"]
-            error = data["score_std"]
+            color = llm_colors[agent_name.removeprefix("LTM ")]
+            score = result["score"]
+            error = result["score_std"]
             if PLOT_STD:
                 kwargs["yerr"] = [[min(error, score)], [min(error, 11 - score)]]
             plt.bar(current_pos, score, color=color, **kwargs)
             current_pos += 1
             num_group_agents += 1
-        x_ticks_labels.append(label)
+        x_ticks_labels.append(span_label)
         x_ticks_pos.append(start_pos + 0.5 * (num_group_agents - 1))
         current_pos += 1
 
     # Manual legend
     plt.legend(handles=[
-        Patch(color=llm_colors[name], label=label)
-        for name, label in zip(llm_names, llm_legend_names)
+        Patch(color=llm_colors[name], label=name) for name in llm_names
     ] + [
         Patch(facecolor="black", label="Only LLM"),
         Patch(facecolor="black", hatch="//", edgecolor="white", label="LLM + LTM"),
